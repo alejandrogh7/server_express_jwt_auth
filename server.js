84d4eb9bd30eaf66519d73ee5b97e89d840babb9
alejandrogh7 <alejo1garciasosa@gmail.com>
2022-12-30 14:21:25 -0500

    middlewares and errors

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3001;
+
+//middleware
+
+//custom logger
+app.use(logger);
+//apply cors
+const whitelist = ["https://localhost:3000"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+//form data -> 'content-type': 'application/form-urlencoded'
+app.use(express.urlencoded({ extended: false }));
+//for json
+app.use(express.json());
+//static files -> CSS
+app.use(express.static(path.join(__dirname, "/public")));
 
 //we could use REGEX in express routes ^/$|/index(.html)?
 //allows / and ( /index.html or /index)
@@ -49,8 +76,18 @@ const three = (req, res, next) => {
 app.get("/chain(.html)?", [one, two, three]);
 
 //No found route
-app.get("/*", (req, res) => {
-  res.sendFile(404, path.join(__dirname, "views", "404.html"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not found" });
+  } else {
+    res.type("txt").send("404 Not found");
+  }
 });
+
+//handler errors
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
